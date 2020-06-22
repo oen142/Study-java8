274d4fd80a7c3cc94abe529fbaa2b6086a41732a
김종완 <oen142@naver.com>
2020-06-22 23:48:30 +0900

    Functional Interface Annotation

--- src/main/java/com/wani/java8study/Function3.java
+++ src/main/java/com/wani/java8study/Function3.java
@@ -1,6 +1,8 @@
 package com.wani.java8study;
 
-public
-interface Function3<T1 ,T2 ,T3 , R>{
+@FunctionalInterface
+public interface Function3<T1 ,T2 ,T3 , R>{
     R apply(T1 t1, T2 t2, T3 t3);
+
+    //void print(int i);
 }
